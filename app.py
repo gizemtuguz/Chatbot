24cba31e185c7babbd1f202b4f3a0ebbfa90d80b
Gizem Tuğuz <gizemtuguz@ogr.eskisehir.edu.tr>
2024-08-12 14:14:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS                     #farklı kaynaklardan gelen istekleri yonetmek için
 from chatbot_application import get_chatbot_response, df_original
 import requests
 import base64
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)                   #uygulamada corsu etkinleştirip istekleri kabule der
 
-# Resim URL'sini indirip base64 formatında döndüren yardımcı fonksiyon
+#resim urlsini indirip base64 formatında döndüren yardımcı fonksiyon
 def fetch_image_as_base64(image_url):
     try:
-        response = requests.get(image_url)
-        response.raise_for_status()  # Hata oluşursa yakala
+        response = requests.get(image_url)              #urlnin içindeki resmi indirir
+        response.raise_for_status()  #hata oluşursa yakalar
         image_bytes = response.content
         base64_image = base64.b64encode(image_bytes).decode('utf-8')
         return f"data:image/jpeg;base64,{base64_image}"
@@ -19,16 +19,16 @@ def fetch_image_as_base64(image_url):
         print(f"Error fetching image: {e}")
         return None
 
-# Chatbot yanıtı
-@app.route('/chatbot', methods=['POST'])
+#porit isteği gelmesi durumunda fonksiyon çalıştırır
+@app.route('/chatbot', methods=['POST'])            
 def chatbot_response():
     user_input = request.json.get('message')
     print(f"Received message: {user_input}")
     
     try:
-        response = get_chatbot_response(user_input, df_original)
+        response = get_chatbot_response(user_input, df_original)               #kullanıcının inputuna en uygun cevabı bulur
         
-        # Eğer yanıt bir img src içeriyorsa, resmi indir ve base64 olarak döndür
+        #eğer yanıt bir img src içeriyorsa resmi indir ve base64 olarak döndür
         if '<img src="' in response:
             start_idx = response.find('<img src="') + len('<img src="')
             end_idx = response.find('"', start_idx)
@@ -36,16 +36,15 @@ def chatbot_response():
             
             base64_image = fetch_image_as_base64(image_url)
             if base64_image:
-                # Resmi base64 formatında döndür
                 response = response.replace(image_url, base64_image)
 
         print(f"Chatbot response: {response}")
         return jsonify({'response': response})
     except Exception as e:
         print(f"Error: {e}")
-        return jsonify({'response': 'An error occurred while processing your request.'}), 500
+        return jsonify({'response': 'An error occurred while processing your request.'}), 500           #hatbotun cevabını json formatına çevirir
 
-@app.route('/test', methods=['GET'])
+@app.route('/test', methods=['GET'])            #test amacıyla get isteği atılır
 def test():
     return "Flask çalışıyor!"
 
